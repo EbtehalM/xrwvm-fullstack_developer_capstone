948c715764f241bb419b259419b90d8805929c20
Ebtehal <Ebtehal.Matar@gmail.com>
2024-05-22 00:15:16 +0300

    python lint

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,8 +1,8 @@
 # Uncomment the following imports before adding the Model code
 
-from django.db import models
-from django.utils.timezone import now
-from django.core.validators import MaxValueValidator, MinValueValidator
+from django.db import models  # type: ignore
+from django.utils.timezone import now  # type: ignore
+from django.core.validators import MaxValueValidator, MinValueValidator  # type: ignore
 
 
 # Create your models here.
@@ -28,27 +28,27 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 class CarMake(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
+
+    # Other fields as needed
+    def __str__(self):
+        return self.name  # Return the name as the string representation
+
+
+class CarModel(models.Model):
+    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
+    # Many-to-One relationship
+    name = models.CharField(max_length=100)
+    CAR_TYPES = [
+        ("SEDAN", "Sedan"),
+        ("SUV", "SUV"),
+        ("WAGON", "Wagon"),
+        # Add more choices as required
+    ]
+    type = models.CharField(max_length=10, choices=CAR_TYPES, default="SUV")
+    year = models.IntegerField(
+        default=2023, validators=[MaxValueValidator(2023), MinValueValidator(2015)]
+    )
     # Other fields as needed
 
     def __str__(self):
         return self.name  # Return the name as the string representation
-
-class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, on_delete=models.CASCADE)  # Many-to-One relationship
-    name = models.CharField(max_length=100)
-    CAR_TYPES = [
-        ('SEDAN', 'Sedan'),
-        ('SUV', 'SUV'),
-        ('WAGON', 'Wagon'),
-        # Add more choices as required
-    ]
-    type = models.CharField(max_length=10, choices=CAR_TYPES, default='SUV')
-    year = models.IntegerField(default=2023,
-        validators=[
-            MaxValueValidator(2023),
-            MinValueValidator(2015)
-        ])
-    # Other fields as needed
-
-    def __str__(self):
-        return self.name  # Return the name as the string representation        
